9ab39d39c839d1022ba4be383e4668333a8b438d
Raeme Khatib <raeme.khatib@telus.com>
2022-11-23 15:19:51 -0700

    finally can move the snake

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.on("data", handleUserInput);
   stdin.setRawMode(true);
@@ -7,12 +10,20 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
+  } if (key == 'w') {
+    connection.write("Move: up");
+  } if (key == 'a') {
+    connection.write("Move: left");
+  } if (key == 's') {
+    connection.write("Move: down");
+  } if (key == 'd') {
+    connection.write("Move: right");
   }
 };
 
 module.exports = {
   setupInput
-}
+};
